9f0a1be029a714405e50ac76282b0d6a111557a7
jongkyu <brane7@naver.com>
2025-11-25 11:14:37 +0900

    no message

---
+++ CMD_Build/temp/programming/packer-driver/targets/editor/chunks/76/762ac39ff33a4272ec85ec6aae2f222d3d123b90.js
@@ -0,0 +1,39 @@
+System.register(["cc"], function (_export, _context) {
+  "use strict";
+
+  var _cclegacy, __checkObsolete__, __checkObsoleteInNamespace__, _decorator, Component, _dec, _class, _crd, ccclass, property, Main;
+
+  return {
+    setters: [function (_cc) {
+      _cclegacy = _cc.cclegacy;
+      __checkObsolete__ = _cc.__checkObsolete__;
+      __checkObsoleteInNamespace__ = _cc.__checkObsoleteInNamespace__;
+      _decorator = _cc._decorator;
+      Component = _cc.Component;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "bd291QOrzZGuImRRBc/XfQD", "Main", undefined);
+
+      __checkObsolete__(['_decorator', 'Component', 'Node']);
+
+      ({
+        ccclass,
+        property
+      } = _decorator);
+
+      _export("Main", Main = (_dec = ccclass('Main'), _dec(_class = class Main extends Component {
+        start() {}
+
+        update(deltaTime) {}
+
+      }) || _class));
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=762ac39ff33a4272ec85ec6aae2f222d3d123b90.js.map
